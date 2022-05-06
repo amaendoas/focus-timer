bb55a0b77e9a6464a971a00263a9f236b033481a
Amanda Guerra <guerraamanda1@hotmail.com>
2022-05-06 09:44:36 -0300

    Primeiro Commit

---
+++ js/index.js
@@ -0,0 +1,39 @@
+// EcmaScript - ES6 Modules
+
+//named import
+import {
+  buttonPlay,
+  buttonPause,
+  buttonStop,
+  buttonSet,
+  buttonSoundOff,
+  buttonSoundOn,
+  minutesDisplay,
+  secondsDisplay
+} from './elements.js'
+//default export
+import Controls from './controls.js'
+import Timer from './timer.js'
+import Sound from './sounds.js'
+import Events from './events.js'
+
+const sound = Sound()
+
+const controls = Controls({
+  buttonPlay,
+  buttonPause,
+  buttonSet,
+  buttonStop
+})
+
+const timer = Timer({
+  minutesDisplay,
+  secondsDisplay,
+  resetControls: controls.reset
+})
+
+Events({
+  controls,
+  timer,
+  sound
+})
